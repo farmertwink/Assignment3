789f7c9309444e89390de6e1837c42dfc564045a
Pj <didona59@students.rowan.edu>
2020-07-21 11:17:16 -0400

    First commit

---
+++ response.js
@@ -0,0 +1,13 @@
+<html> 
+    <head> 
+        <title> PHP sample </title>
+        <style>
+            .name{color:red;}
+            </style>
+    </head>
+
+<body>
+    <p> variable parsing demo </p>
+    <?php 
+        $fname = "Rachael";
+        echo "first name "
